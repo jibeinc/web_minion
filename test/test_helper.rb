45ddddd57a29c0e9fbdec0169dc81f37d120d886
Steven Daniels <sdaniels@jibe.com>
2016-06-05 17:20:43 -0400

    Linting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,17 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 if ENV["CI"]
-  require 'coveralls'
+  require "coveralls"
   Coveralls.wear!
 else
-  require 'simplecov'
+  require "simplecov"
   SimpleCov.start
 end
 
-require 'jibe_ruleset_bot'
-require 'step'
-require 'action'
-require 'flow'
-require 'pry'
-require 'mechanize_bot'
-require 'minitest/autorun'
+require "jibe_ruleset_bot"
+require "step"
+require "action"
+require "flow"
+require "pry"
+require "mechanize_bot"
+require "minitest/autorun"
